87084356805a4f78e7641eb59543596c8300939a
Mahendra Choudhary <jakepintu@gmail.com>
2018-04-15 09:39:48 +0530

    Define UI Vars

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
